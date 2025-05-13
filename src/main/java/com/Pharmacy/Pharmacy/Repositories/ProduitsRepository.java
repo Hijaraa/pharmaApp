d13787771ecb4cd450b3489934a31f8f74fcf03d
Hajar Ou-ra <137091974+Hijaraa@users.noreply.github.com>
2025-05-13 23:53:12 +0100

    Delete src directory

--- src/main/java/com/Pharmacy/Pharmacy/Repositories/ProduitsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Pharmacy.Pharmacy.Repositories;
-
-import com.Pharmacy.Pharmacy.entities.Produits;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProduitsRepository extends JpaRepository<Produits, Long> {
-}
